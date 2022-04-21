8047a9939b1a37afa0dca941d95c125efd4450af
Davi-afk <davi1304@purdue.edu>
2022-04-21 16:50:13 -0400

    Changed sql database and added files for each of the main features so we can work independently

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import tkinter #for gui design and implementation
 import mysql.connector
 from mysql.connector.constants import ClientFlag #for connecting to google cloud
-
+from employees import *
+from movies import *
+from orders import *
 config = {
     'user': 'root',
     'password': '723307009',
-    'host': '104.197.74.226',
+    'host': '34.67.71.254',
     'client_flags': [ClientFlag.SSL],
     'ssl_ca': 'server-ca.pem',
     'ssl_cert': 'client-cert.pem',
@@ -15,57 +17,45 @@ config = {
 config['database'] = 'CS_Cinema'  # add new database to config dict
 cnxn = mysql.connector.connect(**config) #starts connextion
 cursor = cnxn.cursor(buffered=True) #cursor to run commands
-cursor.execute('''DROP TABLE IF EXISTS Employees;''') # how to drop tables
-cursor.execute("CREATE TABLE Employees ("
-               "Employee_ID INTEGER PRIMARY KEY,"
-               "Employee_Name VARCHAR(255),"
-               "Salary INTEGER)") # how to add tables
-cnxn.commit()  # this commits changes to the database
-query = ("INSERT INTO Employees (Employee_ID, Employee_Name, Salary) VALUES(1, 'John', 20000)") #query to inserts some data into employee table
-cursor.execute(query) #inserts data into employee table
-cnxn.commit()  # and commit changes
-cursor.execute("Select * from Employees") #gets all data from employee table
+"""Tables:
+Employees(Employee_ID int PK, Employee_Name char(50), Salary int)
+Theater(Theater_Name char(50) PK, Location char(50))
+Orders(Order_Number int PK, Employee_ID int)
+Items(Order_Number int PK, Food_Name char(50))
+Food(Food_Name char(50) PK, Price float(2), Type char(50))
+Auditoriums(Auditorium_ID int PK, Theater_Name char(50) PK, Capacity int)
+Tickets(Order_Number int PK, Movie_ID int, Showtime time, Ticket_price float(2))
+Movies(Movie_ID int PK, Name char(50), Release_Date date, Runtime time) """
+
+#query = ("CREATE PROCEDURE sp_GetEmployees() BEGIN select Employee_Name,Employee_ID from Employees; END") #query to inserts a stored procedure
+#cursor.execute(query) #inserts stored procedure
+#cnxn.commit()  # and commit changes
+cursor.execute("SHOW TABLES") #gets all tables
 out = cursor.fetchall() #stores query result in variable
 for row in out: #prints out each row to terminal
     print(row)
-
-# Top level window
-frame = tkinter.Tk() #creates a gui window
-frame.title("TextBox Input") #sets window title
-frame.geometry('1000x500') # sets window size
-
-# Function for getting Input from textbox and table results to label widget
-def printInput():
-    inp = inputtxt.get(1.0, "end-1c") #gets input from first box
-    inp2 = inputtxt2.get(1.0, "end-1c") #gets input from second box
-    inp3 = inputtxt3.get(1.0, "end-1c") #gets input from third box
-    # query to insert inputted data into employee table
-    cursor.execute("INSERT INTO Employees (Employee_ID, Employee_Name, Salary) VALUES( \'" + inp + "\' , \'" + inp2 + "\', \'" + inp3 + "\')")
-    cnxn.commit() # commits changes
-    cursor.execute("Select * from Employees") # query to get rows of employee table
-    out = cursor.fetchall()
-    for row in out: #puts each row into label to be displayed in gui
-        lbl = tkinter.Label(frame, text=row)
-        lbl.pack()
-
-# TextBox and their labels Creation
-lbl = tkinter.Label(frame, text="Employee ID")
-lbl.pack()
-inputtxt = tkinter.Text(frame,height=1,width=20)
-inputtxt.pack()
-lbl = tkinter.Label(frame, text="Employee Name")
-lbl.pack()
-inputtxt2 = tkinter.Text(frame,height=1,width=20)
-inputtxt2.pack()
-lbl = tkinter.Label(frame, text="Employee Salary")
-lbl.pack()
-inputtxt3 = tkinter.Text(frame,height=1,width=20)
-inputtxt3.pack()
-
-# Button Creation
-printButton = tkinter.Button(frame, text="Enter", command=printInput)
+frame = tkinter.Tk()  # creates a gui window
+frame.title("TextBox Input")  # sets window title
+frame.geometry('1000x500')  # sets window size
+def employee():
+    frame.destroy()
+    global frame2
+    frame2 = tkinter.Tk()
+    frame2.title("TextBox Input")  # sets window title
+    frame2.geometry('1000x500')
+    printButton = tkinter.Button(frame2, text="Insert", command=insertemp)
+    printButton.pack()
+    printButton2 = tkinter.Button(frame2, text="View", command=employee)
+    printButton2.pack()
+def insertemp():
+    frame2.destroy()
+    insertEmployees(cursor, cnxn)
+printButton = tkinter.Button(frame, text="Movies", command=employee)
 printButton.pack()
-
-# runs gui and closes connection to database
+printButton2 = tkinter.Button(frame, text="Employees", command=employee)
+printButton2.pack()
+printButton3 = tkinter.Button(frame, text="Orders", command=employee)
+printButton3.pack()
 frame.mainloop()
-cnxn.close()
+
+
